06529aaa34eeef2ee9807d2b7b4128424ab60ade
Ruturaj <rutu.bahrain@gmail.com>
2025-06-26 22:24:39 +0530

    idk

--- frontend-project/Components/Footer.tsx
+++ frontend-project/Components/Footer.tsx
@@ -1,49 +1,48 @@
-import React from 'react'
-import MagicButton from './ui/magicbutton'
-import { FaLocationArrow } from 'react-icons/fa'
-import { socialMedia } from '@/data'
-import Image from 'next/image'
+import React from 'react';
+import MagicButton from './ui/magicbutton';
+import { FaLocationArrow } from 'react-icons/fa';
+import { socialMedia } from '@/data';
+import Image from 'next/image';
 
-
-const footer = () => {
+const Footer = () => {
   return (
-    <footer className='w-full pt-20 pb-10 id="contact'>
+    <footer className='w-full pt-20 pb-10' id="contact">
       <div className='w-full absolute left-0 -bottom-72 min-h-96'>
-        <Image src='/footer-grid.svg' alt='grid' className='w-full h-full opacity-100'/>
+        <Image src='/footer-grid.svg' alt='grid' className='w-full h-full opacity-100' />
       </div>
       <div className='flex flex-col items-center'>
         <h1 className='lg:max-w-[45vw]'>
-            Ready to take <span className='text-purple'>your</span> digital presence to the <span className='text-purple'>NEXT LEVEL</span> ?
+          Ready to take <span className='text-purple'>your</span> digital presence to the <span className='text-purple'>NEXT LEVEL</span>?
         </h1>
         <p className='text-white-200 md:mt-10 my-5 text-center'>
-            Reach out to me today and let's discuss how I can help you achieve your goals
+          Reach out to me today and let&apos;s discuss how I can help you achieve your goals
         </p>
-        <a href='mailto: rutu.bahrain@gmail.com'>
-            <MagicButton title="Let's get in touch" 
-            icon = {<FaLocationArrow/>}
+        <a href='mailto:rutu.bahrain@gmail.com'>
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
             position='right'
-            />
+          />
         </a>
-
-
       </div>
       <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
         <p className='md:text-base text-sm md:font-normal font-light p-2'>Copyright © 2024 Ruturaj</p>
         <div className='flex items-center md:gap-3 gap-6'>
-            {socialMedia.map((profile) => (
-                <a key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'
-                  href={profile.link}
-                  target="_blank" rel="noopener noreferrer"
-                >
-                  <Image src={profile.img} alt={profile.id.toString()}
-                  width={20} height={20} />
-                </a>
-            ))}
-
+          {socialMedia.map((profile) => (
+            <a
+              key={profile.id}
+              className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'
+              href={profile.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image src={profile.img} alt={profile.id.toString()} width={20} height={20} />
+            </a>
+          ))}
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default footer
+export default Footer;
